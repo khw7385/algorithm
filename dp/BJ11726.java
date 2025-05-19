7190b147ab91ae3b0335ca0b231e934aa950203b
khw7385 <khw7385@naver.com>
2025-05-19 14:10:20 +0900

    solve 백준 11726

---
+++ dp/BJ11726.java
@@ -0,0 +1,31 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ11726 {
+    private static int N;
+    private static int[] dp; 
+    public static void main(String[] args) throws IOException {
+        readInput();   
+        solve();
+    }
+
+    private static void readInput() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N + 1];
+    }
+
+    private static void solve(){
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3;i <= N; i++){
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        System.out.println(dp[N]);
+    }
+}  
