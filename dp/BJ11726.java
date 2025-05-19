d596f90eedc898c8b7439eba01f40ec599ded624
khw7385 <khw7385@naver.com>
2025-05-19 14:16:29 +0900

    update 백준 11726

--- dp/BJ11726.java
+++ dp/BJ11726.java
@@ -19,10 +19,10 @@ public class BJ11726 {
     }
 
     private static void solve(){
+        dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
 
-        for(int i = 3;i <= N; i++){
+        for(int i = 2;i <= N; i++){
             dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
         }
 
